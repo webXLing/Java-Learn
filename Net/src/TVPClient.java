f659389e4ce3c6a372bd3a7e00452110ac390344
web_xl <597146220@qq.com>
2019-10-24 22:55:55 +0800

    Socket and serverSockrt

---
+++ Net/src/TVPClient.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/*
+    TCP 客户端 ： 向服务器发送链接请求 向服务器发送数据 读取服务器回写数据
+    java.net.Socket 包换端口和ip地址的对象
+ */
+public class TVPClient {
+    public static void main(String[] args) throws IOException {
+//        1.创建一个客户端对象 Socket 绑定ip 和端口
+        Socket socket = new Socket("127.0.0.1",8888);
+//        2.通过socket 客户端对象获取网络字节输出流
+        OutputStream outputStream = socket.getOutputStream();
+
+        // 3.通过write方法 向服务器放松数据
+        outputStream.write("你好服务器".getBytes());
+
+//        4.通过字节输入流获取 服务器响应的数据
+        InputStream inputStream = socket.getInputStream();
+        int len =0;
+        byte[] bytes = new byte[1024];
+        len = inputStream.read(bytes);
+        System.out.println(new String(bytes,0,len));
+//        while ((len=inputStream.read(bytes))!=-1){
+//            System.out.println(new String(bytes,0,len));
+//        }
+//        关闭流
+        socket.close();
+    }
+}
---
+++ Net/src/TVPServer.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+    TCP 服务器 ： 接收客户端发送的请求 向客户端发送数据
+    java.net.ServerSocket 实现服务器的套接字
+ */
+public class TVPServer {
+    public static void main(String[] args) throws IOException {
+//        1.创建ServerSocket 对象 并向系统获取 指定端口
+        ServerSocket server = new ServerSocket(8888);
+        //2 获取请求的客户端
+        Socket accept = server.accept();
+
+//       3.通过socket 客户端对象获取网络字节输入流 读取客户端发送的数据
+        InputStream inputStream = accept.getInputStream();
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        len=inputStream.read(bytes);
+        System.out.println(len);
+
+        System.out.println(new String(bytes,0,len));
+//        while ((len=inputStream.read(bytes))!=-1){ //读取客户端发送的数据
+//            System.out.println(1);
+//            System.out.println(new String(bytes,0,len));
+//        }
+//        System.out.println(11);
+//        4.向客户端 会写数据
+        OutputStream outputStream = accept.getOutputStream();
+        outputStream.write("我是服务器，我收到了".getBytes());
+
+//        关闭流 关闭服务器
+        accept.close();
+        server.close();
+    }
+}
